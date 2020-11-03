f3ce6ba6c05cc93584246bf2eeb6c75ddd9c1304
sumiya11 <60229118+sumiya11@users.noreply.github.com>
2020-11-03 23:45:26 +0300

    in/out format changed

--- src/parsik.jl
+++ src/parsik.jl
@@ -4,7 +4,7 @@ import JSON
 
     For the input format check the file `src/data/matrices_1.json`
 
-    For the possible use case check the file `test/parser_tests.jl`
+    For the possible use case check the file `test/parsik_tests.jl`
 
 =#
 
@@ -15,7 +15,7 @@ function load_matrix_set(filename::String)
 
     open(filepath, "r") do input_stream
         for arr in JSON.Parser.parse(input_stream)
-            push!(ans, transpose(hcat(arr...)))
+            push!(ans, arr)
         end
     end
 
