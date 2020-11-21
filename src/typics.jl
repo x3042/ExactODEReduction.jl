0f379507fe8b2568b34b8e372aeae977a5d4bf35
sumiya11 <60229118+sumiya11@users.noreply.github.com>
2020-11-21 05:20:19 +0300

    added Abstractization/Parametrization
    . now Sparsiks are subtypes of AbstractSparsik
    . now Sparsiks are parameterized with Field

---
+++ src/typics.jl
@@ -0,0 +1,68 @@
+
+
+#=
+    The File contains `AbstractSparsik<T>` interface
+    and related funcs definitions
+=#
+
+#------------------------------------------------------------------------------
+
+import Base: ==, !=, +, -, *
+
+#------------------------------------------------------------------------------
+
+# AbstractSparsik<T>
+#
+# T is a field of coefficients type
+#
+# Base interface for Sparsiks family
+# an Object which implements `AbstractSparsik` can be treated
+# as a vector from a Vector space
+abstract type AbstractSparsik{T} end
+
+#------------------------------------------------------------------------------
+
+function first_nonzero(::T) where {T<:AbstractSparsik} end
+function getindex(::AbstractSparsik, i::Int) end
+
+function scale(::AbstractSparsik, ::C) where {C} end
+function scale!(::AbstractSparsik, ::C) where {C} end
+
+# what if we do not want to extend Base.reduce..
+function Base.reduce(::T, ::T, ::C) where {T<:AbstractSparsik{F}} where {F, C} end
+function reduce!(::T, ::T, ::C) where {T<:AbstractSparsik{F}} where {F, C} end
+
+function inner(::AbstractSparsik) end
+
+# we dont have this for vectors
+function Base.prod(::T, ::T) where {T<:AbstractSparsik{F}} where {F} end
+
+function Base.zero(::AbstractSparsik) end
+function Base.isempty(::AbstractSparsik) end
+function Base.iszero(::AbstractSparsik) end
+
+function dim(::AbstractSparsik) end
+function Base.length(::AbstractSparsik) end
+function Base.size(::AbstractSparsik) end
+function Base.size(::AbstractSparsik, i::Int) end
+
+function density(::AbstractSparsik) end
+
+
+function ==(::T, ::T) where {T<:AbstractSparsik{F}} where {F} end
+function !=(::T, ::T) where {T<:AbstractSparsik{F}} where {F} end
+
+function Base.iterate(::AbstractSparsik) end
+
+function Base.valtype(A::AbstractSparsik) end
+function Base.eltype(A::AbstractSparsik) end
+
+function Base.show(x::AbstractSparsik) end
+function print_sparsik(::AbstractSparsik) end
+
+function modular_reduction(A::AbstractSparsik, field) end
+function rational_reconstruction(A::AbstractSparsik) end
+
+function Base.iterate(A::AbstractSparsik, state) end
+
+#------------------------------------------------------------------------------
