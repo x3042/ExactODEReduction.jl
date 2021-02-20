4511c38a11864cb12899296c95fea5c8312369f6
Alexander Demin <60229118+sumiya11@users.noreply.github.com>
2021-02-20 07:58:46 +0300

    tests updated

--- test/find_basis_tests.jl
+++ test/find_basis_tests.jl
@@ -16,40 +16,69 @@ end
 
 #------------------------------------------------------------------------------
 
-
 # Tests the given algorithm for correctness by comparing
-# it with the output of standard find_basis_1
+# it with the output of standard find_basis_1 on random samples
 # The latter is assumed to be correct
+#
+# Yet some tests are hardcoded just in case
 function test_the_algorithm(algorithm)
 
     # nilpotent space
     set0 = [
-        from_dense([0 1; 0 0], QQ),
+        from_dense([0 1; 0 0;], QQ),
     ]
+    ans0 = linear_span!([
+        from_dense([0 1; 0 0;], QQ)
+    ])
+
     # almost nilpotent space
     set1 = [
-        from_dense([0 1; 0 0], QQ),
-        from_dense([0 0; 1 0], QQ),
+        from_dense([0 1; 0 0;], QQ),
+        from_dense([0 0; 0 1;], QQ),
     ]
+    ans1 = linear_span!([
+        from_dense([0 1; 0 0;], QQ),
+        from_dense([0 0; 0 1;], QQ),
+    ])
+
     # some space
     set2 = [
         from_dense([1 2 3; 0 1 0; 0 0 0;], QQ),
         from_dense([1 0 0; 0 0 0; 0 2 1;], QQ)
     ]
+    ans2 = linear_span!([
+        from_dense([1 0 0; 0 0 0; 0 0 1;], QQ),
+        from_dense([0 1 0; 0 0 0; 0 0 0;], QQ),
+        from_dense([0 0 1; 0 0 0; 0 0 -1//3;], QQ),
+        from_dense([0 0 0; 0 1 0; 0 0 0;], QQ),
+        from_dense([0 0 0; 0 0 0; 0 1 0;], QQ)
+    ])
+
     # complete space
     set3 = [
         from_dense([1 0 0; 2 1 3; 1 0 0;], QQ),
         from_dense([0 1 0; 1 0 0; 0 0 1;], QQ)
     ]
+    ans3 = linear_span!([
+        from_dense([1 0 0; 0 0 0; 0 0 0;], QQ),
+        from_dense([0 1 0; 0 0 0; 0 0 0;], QQ),
+        from_dense([0 0 1; 0 0 0; 0 0 0;], QQ),
+        from_dense([0 0 0; 1 0 0; 0 0 0;], QQ),
+        from_dense([0 0 0; 0 1 0; 0 0 0;], QQ),
+        from_dense([0 0 0; 0 0 1; 0 0 0;], QQ),
+        from_dense([0 0 0; 0 0 0; 1 0 0;], QQ),
+        from_dense([0 0 0; 0 0 0; 0 1 0;], QQ),
+        from_dense([0 0 0; 0 0 0; 0 0 1;], QQ),
+    ])
 
-    for model in [set1, set2, set3]
-        ans = find_basis_1(deepcopy(model))
-        V   = algorithm(deepcopy(model))
-        @test check_inclusion!(ans, V) && check_inclusion!(V, ans)
+    for (set, ans) in zip([set0, set1, set2, set3], [ans0, ans1, ans2, ans3])
+        V = algorithm(deepcopy(set))
+        @test check_inclusion!(deepcopy(ans), V) && check_inclusion!(V, ans)
     end
 
     # --------
     # a series of random tests
+    
     ZZ  = GF(2^31 - 1)
 
     dims      = [3, 3, 4, 4, 5, 5, 5, 5, 8, 8, 8, 8, 10, 10, 10]
