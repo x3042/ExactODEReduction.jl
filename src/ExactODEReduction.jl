b079fbe58d578de735b3e7230403ba77435f0a23
pogudingleb <pogudin.gleb@gmail.com>
2021-08-29 01:15:30 +0200

    Missing file pushed

---
+++ src/ExactODEReduction.jl
@@ -0,0 +1,86 @@
+module ExactODEReduction
+
+
+using DataStructures
+using Distributions
+using JSON
+using OffsetArrays
+using Primes
+using SparseArrays
+using Base.Threads
+
+using AbstractAlgebra
+using Nemo
+
+# These are extended
+import Base: ==, !=, +, -, *, lcm, rand, zero
+import Base.Iterators: Stateful
+import LinearAlgebra: SingularException
+import Nemo.base_ring
+
+import CustomUnitRanges: filename_for_zerorange
+include(filename_for_zerorange)
+
+# Reading input
+include("myeval.jl")
+include("parsik.jl")
+
+# Utilities
+include("utils.jl")
+include("typics.jl")
+include("gizmos.jl")
+
+# Structures for sparse linear algebra
+include("structs/sparsik.jl")
+include("structs/dok_sparsik.jl")
+include("structs/csr_sparsik.jl")
+include("structs/densik.jl")
+include("structs/subspacik.jl")
+
+# Main functionality
+include("wiedemannchik.jl")
+include("basis.jl")
+include("radical.jl")
+include("invariants.jl")
+
+
+
+# finds one common invariant subspace of the given matrices
+# using the provived default method for finding one invaiant subspace
+"""
+    invariant_subspace(As; backend_algorithm)
+
+Finds *one* common invariant subspace of the given matrices `As`
+using the provived default method `backend_algorithm`. By default, `invariant_subspace_1`
+is used.
+
+Each matrix in `As` to be a subtype of `AbstractSparseMatrix`
+"""
+function invariant_subspace(
+    As;
+    backend_algorithm=invariant_subspace_1)
+
+    backend_algorithm(As)
+end
+
+#------------------------------------------------------------------------------
+
+# finds several common invariant subspaces of the given matrices
+# using the provided default method for finding one invaiant subspace
+"""
+    many_invariant_subspaces(As; backend_algorithm)
+
+Finds *several* common invariant subspaces of the given matrices `As`
+using the provived default method `backend_algorithm`. By default, `invariant_subspace_1`
+is used.
+
+Each matrix in `As` to be a subtype of `AbstractSparseMatrix`
+"""
+function many_invariant_subspaces(
+    As;
+    backend_algorithm=invariant_subspace_1)
+
+    __many_invariant_subspaces(As, backend_algorithm)
+end
+
+end
