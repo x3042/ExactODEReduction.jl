39b8c565e9b7cffc6fa3bc2391dcb6bd52049a87
Alexander Demin <asdemin_2@edu.hse.ru>
2022-08-30 01:27:11 +0200

    wuwu

--- experimental/matmul.jl
+++ experimental/matmul.jl
@@ -6,16 +6,29 @@ using SparseArrays
 
 include("../src/ExactODEReduction.jl")
 
-nnz, total = 5, 100
 n = 100
-Adense = QQ.(div.(rand(1:total, n, n), total-nnz))
-# Adense contains ~5% nonzeros
+total = 100
 
-Acsr = sparse(Adense)
-Asparsik = ExactODEReduction.from_dense(Adense, QQ)
+for nnz in [1, 5, 15]
+    Adense = QQ.(div.(rand(1:total, n, n), total-nnz))
+    # Adense contains ~nnz% nonzeros
 
-@btime *($Acsr, $Acsr);
-# 189.400 μs (3401 allocations: 153.72 KiB)
+    Acsr = sparse(Adense)
+    Asparsik = ExactODEReduction.from_dense(Adense, QQ)
 
-(@btime prod($Asparsik, $Asparsik); true)
-# 6.133 ms (26853 allocations: 1.16 MiB)
+    @btime *($Acsr, $Acsr);
+    # nnz = 1
+    # 27.500 μs (441 allocations: 23.94 KiB)
+    # nnz = 5
+    # 189.400 μs (3401 allocations: 153.72 KiB)
+    # nnz = 15
+    # 1.614 ms (38674 allocations: 1.33 MiB)
+
+    (@btime prod($Asparsik, $Asparsik); true)
+    # nnz = 1
+    # 1.671 ms (13169 allocations: 471.30 KiB)
+    # nnz = 5
+    # 6.133 ms (26853 allocations: 1.16 MiB)
+    # nnz = 15
+    # 17.515 ms (85495 allocations: 3.38 MiB)
+end
